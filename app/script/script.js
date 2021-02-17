69aed492c653e6c78353538557381b653e5116fb
mriliya <mr.litvishchenko@gmail.com>
2021-02-17 22:37:21 +0200

    added modal window

--- app/script/script.js
+++ app/script/script.js
@@ -1,5 +1,20 @@
 const menuBtn = document.querySelector('.mobile-menu__btn')
-const mobileMenu=document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const modalOpen = document.querySelectorAll('.modal-open')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal__close')
+
 menuBtn.addEventListener('click', () => {
 	mobileMenu.classList.toggle('active')
+})
+
+modalOpen.forEach(element => {
+	element.addEventListener('click', (e) => {
+		e.preventDefault()
+		modal.classList.add('active')
+	})
+})
+
+modalClose.addEventListener('click', () => {
+	modal.classList.remove('active')
 })
